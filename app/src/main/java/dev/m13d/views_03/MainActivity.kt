9c93cca78ca067d36cebf6fcdba6a39d84c5d8ff
Michael <3yebmb@gmail.com>
2022-02-19 01:32:14 +0400

    add: switch between flags by click button

--- app/src/main/java/dev/m13d/views_03/MainActivity.kt
+++ app/src/main/java/dev/m13d/views_03/MainActivity.kt
@@ -1,11 +1,48 @@
 package dev.m13d.views_03
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var customLayout: LinearLayout
+    private var counter = 0
+    private val listLayout = listOf(
+        R.layout.layout_austria,
+        R.layout.layout_poland,
+        R.layout.layout_italy,
+        R.layout.layout_columbia,
+        R.layout.layout_madagascar,
+        R.layout.layout_thailand,
+        R.layout.layout_thailand,
+        R.layout.layout_switzerland
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        customLayout = findViewById(R.id.custom_layout)
+        findViewById<Button>(R.id.btn_next).setOnClickListener {
+            if (counter < listLayout.size)
+                counter++
+            else
+                counter = 0
+            changeView(changeLayout(listLayout[counter]))
+        }
+        changeView(changeLayout(listLayout[counter]))
     }
+
+    private fun changeLayout(id: Int) : View =
+        LayoutInflater.from(this).inflate(id, null, false)
+
+    private fun changeView(view: View) {
+        customLayout.removeAllViews()
+        customLayout.addView(view)
+    }
+
 }
