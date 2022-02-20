9493373998c60cc6bbec68bbe9a7916964ec767d
Michael <3yebmb@gmail.com>
2022-02-21 00:37:00 +0400

    add: load image from remote resource

--- app/src/main/java/dev/m13d/pictureloader/MainActivity.kt
+++ app/src/main/java/dev/m13d/pictureloader/MainActivity.kt
@@ -1,14 +1,17 @@
 package dev.m13d.pictureloader
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import dev.m13d.pictureloader.databinding.ActivityMainBinding
+import java.io.FileNotFoundException
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var url: URL
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +19,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonRequest.setOnClickListener {
+            loader(binding.adressInput.text.toString())
+        }
+    }
 
+    private fun loader(input: String) {
+        val url = URL(BASE_URL + input + URL_TERMINATOR)
+        Thread {
+            try {
+                val bitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+                binding.imageViewer.post {
+                    binding.imageViewer.setImageBitmap(bitmap)
+                }
+            } catch (e: FileNotFoundException) {
+                binding.imageViewer.post {
+                    binding.imageViewer.setImageResource(0)
+                }
+                showToast(ERROR)
+            }
+        }.start()
+    }
+
+    private fun showToast(message: String?) {
+        runOnUiThread {
+            val toast = Toast.makeText(this, message, Toast.LENGTH_LONG)
+            toast.setGravity(Gravity.CENTER, 0, 0)
+            toast.show()
         }
     }
 
     companion object {
         const val BASE_URL = "https://picsum.photos/id/"
-        const val URL_TERMINATOR = "//320//320"
+        const val URL_TERMINATOR = "/320/320"
+        const val ERROR = """
+            You put incorrect picture id.
+                Please try again. 
+        """
     }
 }
