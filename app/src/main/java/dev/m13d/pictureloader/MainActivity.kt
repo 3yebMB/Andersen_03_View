7555e45406638b3775497c11228071dc51160871
Michael <3yebmb@gmail.com>
2022-02-20 22:39:42 +0400

    add: base constants

--- app/src/main/java/dev/m13d/pictureloader/MainActivity.kt
+++ app/src/main/java/dev/m13d/pictureloader/MainActivity.kt
@@ -1,11 +1,27 @@
 package dev.m13d.pictureloader
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.m13d.pictureloader.databinding.ActivityMainBinding
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var url: URL
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonRequest.setOnClickListener {
+
+        }
+    }
+
+    companion object {
+        const val BASE_URL = "https://picsum.photos/id/"
+        const val URL_TERMINATOR = "//320//320"
     }
 }
