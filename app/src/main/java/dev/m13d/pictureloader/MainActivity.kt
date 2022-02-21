4a0fd04f3add591f8889408d7219c81b8ca75fa4
Michael <3yebmb@gmail.com>
2022-02-22 00:12:05 +0400

    add: loding image by glide

--- app/src/main/java/dev/m13d/pictureloader/MainActivity.kt
+++ app/src/main/java/dev/m13d/pictureloader/MainActivity.kt
@@ -1,7 +1,16 @@
 package dev.m13d.pictureloader
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import dev.m13d.pictureloader.databinding.ActivityMainBinding
 import java.net.URL
 
@@ -16,12 +25,52 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonRequest.setOnClickListener {
-
+            loader(BASE_URL + binding.adressInput.text.toString() + URL_TERMINATOR)
         }
+    }
+
+    private fun loader(imageUrl: String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(imageUrl)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .listener(object : RequestListener<Bitmap> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Bitmap>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    showError()
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Bitmap?,
+                    model: Any?,
+                    target: Target<Bitmap>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.imageViewer.setImageBitmap(resource)
+                    return true
+                }
+            })
+            .into(binding.imageViewer)
+    }
+
+    private fun showError() {
+        val toast = Toast.makeText(this, ERROR, Toast.LENGTH_LONG)
+        toast.setGravity(Gravity.CENTER, 0, 0)
+        toast.show()
     }
 
     companion object {
         const val BASE_URL = "https://picsum.photos/id/"
         const val URL_TERMINATOR = "//320//320"
+        const val ERROR = """
+            You put incorrect picture id.
+                Please try again. 
+        """
     }
 }
