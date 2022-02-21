24724c2fab90a73cefa4be26ab6ea6c401965dde
Michael <3yebmb@gmail.com>
2022-02-21 16:24:29 +0400

    add: download image by downloadmanager & show it after

--- app/src/main/java/dev/m13d/pictureloader/MainActivity.kt
+++ app/src/main/java/dev/m13d/pictureloader/MainActivity.kt
@@ -1,14 +1,30 @@
 package dev.m13d.pictureloader
 
+import android.Manifest
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.view.Gravity
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import dev.m13d.pictureloader.databinding.ActivityMainBinding
+import java.io.File
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var url: URL
+    private var downloaded: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +32,69 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonRequest.setOnClickListener {
-
+            loader(binding.adressInput.text.toString())
         }
+    }
+
+    private fun loader(id: String) {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+            || ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ),
+                1
+            )
+        } else {
+            val downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+            val uri = Uri.parse(BASE_URL + id + URL_TERMINATOR)
+            val request = DownloadManager.Request(uri)
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "$id.jpg")
+            downloaded = downloadManager.enqueue(request)
+        }
+
+        val br = object : BroadcastReceiver() {
+            override fun onReceive(p0: Context?, p1: Intent?) {
+                val id = p1?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+                if (id == downloaded) {
+                    val path = Environment.DIRECTORY_DOWNLOADS.toString() + "/${binding.adressInput.text}.jpg"
+                    val imgFile = File(path)
+                    if (imgFile.exists()) {
+                        val myBitmap = BitmapFactory.decodeFile(imgFile.absolutePath)
+                        binding.imageViewer.setImageBitmap(myBitmap)
+                    } else
+                        Log.d("BroadcastReceiver", "$id")
+                } else {
+                    showError()
+                }
+            }
+        }
+        registerReceiver(br, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+    }
+
+    private fun showError() {
+        val toast = Toast.makeText(this, ERROR, Toast.LENGTH_LONG)
+        toast.setGravity(Gravity.CENTER, 0, 0)
+        toast.show()
     }
 
     companion object {
         const val BASE_URL = "https://picsum.photos/id/"
         const val URL_TERMINATOR = "//320//320"
+        const val ERROR = """
+            You put incorrect picture id.
+                Please try again. 
+        """
     }
 }
